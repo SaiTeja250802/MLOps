a40b46234f76f6693916e85558625db6e017a960
SaiTeja <saiteja.amgoth@gmail.com>
2024-05-18 01:23:40 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
-from setuptools import find_packages, setup
+# from setuptools import find_packages, setup
 
-setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-<<<<<<< HEAD
-    description='Performing MLOps',
-=======
-    description='A project on Mlops',
->>>>>>> origin/main
-    author='SaiTeja',
-    license='MIT',
-)
+# setup(
+#     name='src',
+#     packages=find_packages(),
+#     version='0.1.0',
+#     description='Performing MLOps',
+#     description='A project on Mlops',
+#     author='SaiTeja',
+#     license='MIT',
+# )
+
+# NOTE: For windows user-
+# This file must be created in the root of the project 
+# where Training and Prediction batch file as are present
+
+import os
+from glob import glob
+
+
+data_dirs = ["Training_Batch_Files","Prediction_Batch_files"]
+
+for data_dir in data_dirs:
+    files = glob(data_dir + r"/*.csv")
+    for filePath in files:
+        # print(f"dvc add {filePath}")
+        os.system(f"dvc add {filePath}")
+
+print("\n #### all files added to dvc ####")
